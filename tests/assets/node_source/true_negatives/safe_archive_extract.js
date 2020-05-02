c77377f45f94cbe68508d9242921225c9467fd28
Ajin Abraham <ajin25@gmail.com>
2020-05-02 14:45:27 -0400

    zip extract path overwrite

---
+++ tests/assets/node_source/true_negatives/safe_archive_extract.js
@@ -0,0 +1,27 @@
+
+entry.pipe(fs.createWriteStream(fileName));
+
+const fs = require('fs');
+const unzip = require('unzip');
+const path = require('path');
+
+fs.createReadStream('archive.zip')
+    .pipe(unzip.Parse())
+    .on('entry', entry => {
+        const fileName = entry.path;
+        entry.pipe(fs.createWriteStream(path.join('my_directory', path.basename(fileName))));
+    });
+
+fs.createReadStream('archive.zip')
+    .pipe(unzip.Parse())
+    .on('entry', entry => {
+        const fileName = entry.path;
+        var x = fileName.indexOf('..');
+        if (fileName.indexOf('..') == -1) {
+            // sgrep limitation
+            //entry.pipe(fs.createWriteStream(fileName));
+        }
+        else {
+            console.log('skipping bad path', fileName);
+        }
+    });
