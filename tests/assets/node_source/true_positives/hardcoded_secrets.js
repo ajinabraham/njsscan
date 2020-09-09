0e33ed41695ad6cdb25ed7694ab426d571648d04
Ajin Abraham <ajin25@gmail.com>
2020-09-08 22:13:52 -0400

    metavar-regex for hardcoded secrets

--- tests/assets/node_source/true_positives/hardcoded_secrets.js
+++ tests/assets/node_source/true_positives/hardcoded_secrets.js
@@ -1,7 +1,11 @@
 // ruleid:node_password
 password = '1212';
+foo = "adasd";
 x = 1;
 password = x;
+password = '';
+x.password = 123
+x["password"] = 1;
 pass = 123;
 // ruleid:node_password
 PASSWORD = '12211';
@@ -23,6 +27,23 @@ let password = '1212';
 var password = '1212';
 
 // ruleid:node_api_key
-angular.module('starter.services', [])
-    .constant('api_key', '6e906986c3b199c51fff3154cfb76979')
+angular.module('starter.services', []).constant('api_key', '6e906986c3b199c51fff3154cfb76979')
 this.apiUrl = api_url;
+// ruleid:node_secret
+aws('secret', 'asdasdadasddd')
+// ruleid:node_api_key
+x.config('APIKEY', 'asdsdadsada')
+// ruleid:node_api_key
+api_key = "213123123123";
+// ruleid:node_api_key
+api["apikey"] = "asddadasddad"
+// ruleid:node_api_key
+obj('APIKEY') = 'asdasdsadasdad'
+// ruleid:node_api_key
+obj('api_key') = 'asdasdsadasdad'
+// ruleid:node_api_key
+obj('Api_Key') = 'asdasdsadasdad'
+// ruleid:node_secret
+secret = "Asdadasdasdda"
+// ruleid:node_secret
+obj.secret = "Asdadasdasddsa"
