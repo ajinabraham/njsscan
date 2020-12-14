0ebedb3c910afae50cb2654c9e40483e3a53de8b
Ajin Abraham <ajin25@gmail.com>
2020-12-13 21:19:19 -0500

    remove function name clobbering

--- njsscan/formatters/json.py
+++ njsscan/formatters/json.py
@@ -6,14 +6,14 @@ import json
 def json_output(outfile, scan_results, version):
     """JSON Output."""
     scan_results['njsscan_version'] = version
-    json_output = json.dumps(
+    jout = json.dumps(
         scan_results,
         sort_keys=True,
         indent=2,
         separators=(',', ': '))
     if outfile:
         with open(outfile, 'w') as of:
-            of.write(json_output)
+            of.write(jout)
     else:
-        print(json_output)
-    return json_output
+        print(jout)
+    return jout
